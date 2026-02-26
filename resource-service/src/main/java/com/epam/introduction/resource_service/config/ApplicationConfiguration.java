0251111fb6477d12825bbb18fbc3f7bf3b0a2df3
Kirill Sachevko <45870088+kirillsachevko@users.noreply.github.com>
2026-02-26 10:11:28 +0200

    service-discovery/gateway impl

--- resource-service/src/main/java/com/epam/introduction/resource_service/config/ApplicationConfiguration.java
+++ resource-service/src/main/java/com/epam/introduction/resource_service/config/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.epam.introduction.resource_service.config;
 import org.apache.tika.parser.mp3.Mp3Parser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +22,7 @@ public class ApplicationConfiguration {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
